ee31829eb5aa1e692491577417dce2abdf03c22e
Frans van der Zijde <vanderzijde@ubib.eur.nl>
2017-08-11 15:03:31 +0200

    Add filter functionality

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,6 +18,37 @@ module.exports = {
 
       }
       return $.isArray(todos) ? todos : [];
+   },
+   filteredTodos: function (todos, showCompleted, searchText) {
+      var filteredTodos = todos;
+
+      // Filter by showCompleted
+      filteredTodos = filteredTodos.filter((todo) => {
+         // Show only todos who not been completed or show all
+         // todos when the checkbox showCompleted is checked.
+         return !todo.completed || showCompleted;
+      });
+
+      // Filter by searchText
+      filteredTodos = filteredTodos.filter((todo) => {
+         var text = todo.text.toLowerCase();
+         return searchText.length === 0 || text.indexOf(searchText) > -1;
+      });
+
+      // Sort todo with non-completed first
+      filteredTodos.sort((a, b) => {
+         if ( !a.completed && b.completed)
+            return -1;
+         else if ( a.completed && !b.completed)
+            return 1;
+         else
+            return 0;
+         // return -1; // a < b
+         // return 1; // a > b
+         // return 0; // a === b
+      });
+
+      return filteredTodos;
    }
 
 };
